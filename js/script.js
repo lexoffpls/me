6351670503e857938e2803125a5bc3aa65558ce8
Лёха Лексус <105968273+lexoffpls@users.noreply.github.com>
2023-05-02 05:17:10 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,72 +1,99 @@
-if (document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", function () {
-        loaded();
+
+
+
+$(document).ready(function() {
+
+
+    
+    
+  $('.window').children('.buttons').mouseenter(function(){
+    $(this).children('.buttons_hover_yes').show();
+  });
+
+  $('.window').children('.buttons').mouseleave(function(){
+    $(this).children('.buttons_hover_yes').css("display","none")
+  });
+
+
+  $('.button-close').click(function(){
+    $(this).parents().parents('.window').css("display","none")
+      
+ 
+    
+    if ($(this).parents().parents('.window').attr("id") == "window-3") $("#window-3 .window-content").attr("src", "");
+    $('.show').show();
+      
+    
+  });
+
+  $('.show').click(function(){
+    $('.window').show();
+    $(this).hide()
+  });
+
+  $(function() {
+    $(".window").draggable({handle: ".handle"});
+    $(".folder").draggable();
+    $(".window").resizable({
+      minWidth: 300,
+      minHeight: 200,
+      create: function( event, ui ) {
+          $(".ui-resizable-s").css("cursor","ns-resize");
+          $(".ui-resizable-se").css("cursor","nwse-resize");
+          $(".ui-resizable-e").css("cursor","ew-resize");
+      },
+      resize: function(event, ui ){
+        var properHeight = $(".column-1").width();
+        $(".column-2").height("100%");
+        $(".column-3").height("100%");
+     
+        $(".column-2-holder").height("100%");
+        $(".column-3-holder").height("100%");
+      }
     });
-} else if (document.attachEvent) {
-    document.attachEvent("onreadystatechange", function () {
-        loaded();
+    $(".column-1").resizable({
+      containment: ".content-area",
+      minWidth: 130,
+      handles: "e",
+        create: function( event, ui ) {
+    
+            $(".ui-resizable-e").css("cursor","ew-resize");
+        },
+        resize: function (event, ui){
+          $(".column-2-holder").css("left",$(".column-1").width()+"px");
+          $(".column-3-holder").css("left",$(".column-2").width()+"px");
+          $(".column-4-holder").css("left",$(".column-3").width()+"px");
+        }
+      });
+    $(".column-2-holder").css("left","150px");
+    $(".column-2-holder").resizable({
+      containment: ".content-area",
+      alsoResize: ".column-2",
+      minWidth: 150,
+      handles: "e",
+        create: function( event, ui ) {
+       
+            $(".ui-resizable-e").css("cursor","ew-resize");
+        },
+        resize: function (event, ui) {
+            $(".column-3-holder").css("left",$(".column-2").width()+"px");
+            $(".column-4-holder").css("left",$(".column-3").width()+"px");
+        }
+      });
+    $(".column-3-holder").css("left","150px");
+    $(".column-3-holder").resizable({
+      containment: ".content-area",
+      alsoResize: ".column-3",
+      minWidth: 150,
+      handles: "e",
+        create: function( event, ui ) {
+         
+            $(".ui-resizable-e").css("cursor","ew-resize");
+        },
+        resize: function (event, ui) {
+            $(".column-4-holder").css("left",$(".column-3").width()+"px");
+        }
+      });
+
     });
-}
-
-function loaded() {
-
-    setInterval(loop, 300);
-
-}
-
-var x = 0;
-
-var titleText = ["|",
-					"★…",
-					"★… R",
-					"★… Re",
-					"★… Res",
-					"★… Reso",
-					"★… Resol",
-					"★… Resolv",
-					"★… Resolve",
-                    "★… ResolveL",
-                    "★… ResolveLe",
-                    "★… ResolveLes",
-                    "★… ResolveLess",
-                    "★… ResolveLess ",
-                    "★… ResolveLess …★",
-                    "★… ResolveLess ★",
-                    "★… ResolveLess ",
-                    "★… ResolveLess",
-                    "★… ResolveLes",
-                    "★… ResolveLe",
-                    "★… ResolveL",
-                    "★… Resolve",
-                    "★… Resolv",
-                    "★… Resol",
-                    "★… Reso",
-                    "★… Res",
-                    "★… Re",
-                    "★… R",
-                    "★…",
-                    "★",
-                    "|",
-                    "★…", ];
-
-
-function loop() {
-    document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
-}
-
-$(window).on("scroll", function () {
-    var a = $("#header");
-    200 < $(window).scrollTop() ? a.addClass("sticky_navigation") : a.removeClass("sticky_navigation")
-}), $("#wrapper-dropdown").click(function (a) {
-    $(this).hasClass("active") ? $(this).removeClass("active") : $(this).addClass("active")
-}), $("a[href^='#']").click(function (a) {
-    a.preventDefault();
-    var s = $(this).attr("href");
-    return $("html, body").animate({
-        scrollTop: $(s).offset().top + "px"
-    }, 2e3, function () {
-        "#home" == s && ($(".navbar-auth").removeClass("advantages-a buy-a support-a").addClass("home-a"), $(".navbar-lang").removeClass("lang-advantages lang-buy lang-support").addClass("lang-home")), "#advantages" == s && ($(".navbar-auth").removeClass("home-a buy-a support-a").addClass("advantages-a"), $(".navbar-lang").removeClass("lang-home lang-buy lang-support").addClass("lang-advantages")), "#buy" == s && ($(".navbar-auth").removeClass("home-a advantages-a support-a").addClass("buy-a"), $(".navbar-lang").removeClass("lang-home lang-advantages lang-support").addClass("lang-buy")), "#support" == s && ($(".navbar-auth").removeClass("home-a advantages-a buy-a").addClass("support-a"), $(".navbar-lang").removeClass("lang-home lang-advantages lang-buy").addClass("lang-support"))
-    }), !1
-}), $(".support-item").click(function (a) {
-    $(this).is("#hw-1") && ($("#1").addClass("active-win"), $("#2").removeClass("active-win"), $("#3").removeClass("active-win"), $("#4").removeClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-2") && ($("#1").removeClass("active-win"), $("#2").addClass("active-win"), $("#3").removeClass("active-win"), $("#4").removeClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-3") && ($("#1").removeClass("active-win"), $("#2").removeClass("active-win"), $("#3").addClass("active-win"), $("#4").removeClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-4") && ($("#1").removeClass("active-win"), $("#2").removeClass("active-win"), $("#3").removeClass("active-win"), $("#4").addClass("active-win"), $("#5").removeClass("active-win")), $(this).is("#hw-5") && ($("#1").removeClass("active-win"), $("#2").removeClass("active-win"), $("#3").removeClass("active-win"), $("#4").removeClass("active-win"), $("#5").addClass("active-win")), $(this).is("#hw-6") && window.open("https://vk.com/qn1s_it")
-})
+});
